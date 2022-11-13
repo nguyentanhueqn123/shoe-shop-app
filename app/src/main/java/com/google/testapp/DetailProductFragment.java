ad60650ce676613e339d126d78ad2adec537ad99
Huệ Nguyễn Tấn <20521359@ms.uit.edu.vn>
2022-11-14 00:05:00 +0700

    slide img and detail buy now

--- app/src/main/java/com/google/testapp/DetailProductFragment.java
+++ app/src/main/java/com/google/testapp/DetailProductFragment.java
@@ -1,13 +1,30 @@
 package com.google.testapp;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,12 +73,129 @@ public class DetailProductFragment extends Fragment {
         }
     }
 
+
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
+    private PhotoAdapter photoAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view1 = inflater.inflate(R.layout.fragment_detail_product, container, false);
 
+        // Dialog Filter
+        TextView tvBuyNow = view1.findViewById(R.id.tvBuyNow);
+        tvBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBuyNowDialog(Gravity.BOTTOM);
+
+            }
+        });
+        // Xử lý nút back
+        ((ImageView)view1.findViewById(R.id.btnBackDetailProduct)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        viewPager = view1.findViewById(R.id.viewpager);
+        circleIndicator = view1.findViewById(R.id.circle_indicator);
+
+        photoAdapter = new PhotoAdapter(this.getContext(), getListPhoto());
+        viewPager.setAdapter(photoAdapter);
+
+        circleIndicator.setViewPager(viewPager);
+        photoAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+
+
+
+
+
         return view1;
     }
+    private List<Photo> getListPhoto() {
+        List<Photo> list = new ArrayList<>();
+        list.add(new Photo(R.drawable.nike));
+        list.add(new Photo(R.drawable.nikev2));
+        list.add(new Photo(R.drawable.nikev3));
+        list.add(new Photo(R.drawable.nikev4));
+
+
+        return list;
+    }
+    int sl;
+
+    private void openBuyNowDialog(int gravity){
+        final Dialog dialog = new Dialog(this.getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_buy_product);
+
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAtrributes = window.getAttributes();
+        windowAtrributes.gravity = gravity;
+        window.setAttributes(windowAtrributes);
+
+        if(Gravity.BOTTOM ==gravity){
+            dialog.setCancelable(true);
+        }
+        else {
+            dialog.setCancelable(false);
+        }
+
+
+        ImageView btnBackBuyNow = dialog.findViewById(R.id.btnBackBuyNow);
+        btnBackBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+//        Button btnApplyFilter = dialog.findViewById(R.id.btnApplyFilter);
+//        btnApplyFilter.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//                Toast.makeText(getContext(), "Apply Filter is successful", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        TextView count = dialog.findViewById(R.id.tvQuantity);
+        ImageView add = dialog.findViewById(R.id.btnAdd);
+        ImageView remove = dialog.findViewById(R.id.btnRemove);
+        sl =1;
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sl++;
+                count.setText(String.valueOf(sl));
+            }
+        });
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sl>1)
+                {
+                    sl--;
+                    count.setText(String.valueOf(sl));
+                }
+            }
+        });
+
+
+        dialog.show();
+
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/google/testapp/Photo.java
@@ -0,0 +1,18 @@
+package com.google.testapp;
+
+public class Photo {
+    private int resourceId;
+
+
+    public Photo(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+}
